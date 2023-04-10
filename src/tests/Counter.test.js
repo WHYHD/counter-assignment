976e0886f4c5d59d1a29f38e247a4704e481b702
Nicholas <charnick60@gmail.com>
2023-04-09 21:57:48 -0400

    Test done

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,47 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
+import userEvent from '@testing-library/user-event';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter setCount={0}/>);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const cM = screen.getByText(/Counter/i);
+  expect(cM).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const iZ = screen.getByText(/0/i);
+  //const x = screen.getByTestId('data-testid');
+  expect(iZ).toBeInTheDocument();
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByText('+');
+  const countElement = screen.getByText(/0/i);
+
+  expect(countElement).toBeInTheDocument;
+
+  
+  fireEvent.click(incrementButton);
+
+  const countElement1 = screen.getByText(/1/i);
+  expect(countElement1).toBeInTheDocument;
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const dB = screen.getByText('-');
+  const countElement = screen.getByText(/0/i);
+
+  expect(countElement).toBeInTheDocument;
+
+  fireEvent.click(dB);
+
+  const countElement2 = screen.getByText(/-1/i);
+
+  expect(countElement2).toBeInTheDocument;
 });
